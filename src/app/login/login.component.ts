da5d07b747e40ced7cf2e1c0ff2292c677abf4fc
emraan-abbas <imranabbas67@gmail.com>
2022-06-07 23:20:19 +0500

    Login

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -1,6 +1,9 @@
 import { Component, OnInit } from '@angular/core';
-
+import { UserService } from '../Services/user.service';
 import { FormControl, FormGroup, Validators } from '@angular/forms';
+import { catchError } from 'rxjs';
+import { JwtServiceService } from '../Services/jwt-service.service';
+import  {Router} from '@angular/router';
 
 @Component({
   selector: 'app-login',
@@ -9,26 +12,54 @@ import { FormControl, FormGroup, Validators } from '@angular/forms';
 })
 export class LoginComponent implements OnInit {
 
-  constructor() { }
+  constructor(
+    private userService: UserService, 
+    private jwtService: JwtServiceService,
+    private router: Router
+    ) { }
 
   ngOnInit(): void {
   }
 
+
+  // Log In Form
   loginForm = new FormGroup({
     email: new FormControl('', [Validators.required, Validators.email]),
     password: new FormControl('', [Validators.required])
   });
 
+  resData: any;
+  token:any;
+  userRole: any;
+
+  // Log In Function
   login() {
-    console.log(this.loginForm.value)
-  }
+    return this.userService.logIn(this.loginForm.value)
+    .pipe(
+      catchError(err => {
+        console.log('Error :', err);
+        return err;
+      })
+    ).subscribe(res =>{
+      this.resData = res
+      this.token = this.resData.token
+      localStorage.setItem('token', this.resData.token)
+      this.userRole = this.jwtService.DecodeToken(this.token)
+      this.router.navigateByUrl('/home/crd')
+      }
+    )
+  };
+
+
+  
+  // Validators here
 
   get emailValidator() {
     return this.loginForm.get('email');
-  }
+  };
 
   get passwordValidator() {
     return this.loginForm.get('password');
-  }
+  };
 
 }
