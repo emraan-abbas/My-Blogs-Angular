abb49441aca3e2ccd7a52ce4c5e1925f0134fbe6
emraan-abbas <imranabbas67@gmail.com>
2022-06-07 23:13:37 +0500

    New Modules Added

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -12,18 +12,31 @@ import { ReactiveFormsModule } from '@angular/forms';
 import { LoginComponent } from './login/login.component';
 import { AllUsersComponent } from './all-users/all-users.component';
 
-// import { CdkColumnDef } from '@angular/cdk/table';
 
 import {MatInputModule} from '@angular/material/input';
 import {MatButtonModule} from '@angular/material/button';
 import {MatTableModule} from '@angular/material/table';
+import {MatCardModule} from '@angular/material/card';
+import {MatDialogModule} from '@angular/material/dialog';
+
+import { JwtServiceService } from './Services/jwt-service.service';
+import { HomeComponent } from './home/home.component';
+import { BlogCrdComponent } from './home/blog-crd/blog-crd.component';
+import { BlogExpComponent } from './home/blog-exp/blog-exp.component';
+import { CreateBlogComponent } from './home/create-blog/create-blog.component';
+import { AdminHomeComponent } from './admin-home/admin-home.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     CreateUserComponent,
     LoginComponent,
-    AllUsersComponent
+    AllUsersComponent,
+    HomeComponent,
+    BlogCrdComponent,
+    BlogExpComponent,
+    CreateBlogComponent,
+    AdminHomeComponent
   ],
   imports: [
     BrowserModule,
@@ -36,9 +49,12 @@ import {MatTableModule} from '@angular/material/table';
     MatInputModule,
     MatButtonModule,
     MatTableModule,
+    MatCardModule,
+    MatDialogModule
   ],
   providers: [
     // CdkColumnDef
+    JwtServiceService
   ],
   bootstrap: [AppComponent]
 })
