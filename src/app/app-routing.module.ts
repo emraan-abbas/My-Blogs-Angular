1475ceb93aacb88c77928d0036b2893b1c2304ca
emraan-abbas <imranabbas67@gmail.com>
2022-06-07 23:13:24 +0500

    Routes Added

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -4,19 +4,51 @@ import { RouterModule, Routes } from '@angular/router';
 import {CreateUserComponent} from './create-user/create-user.component';
 import {LoginComponent} from './login/login.component';
 import {AllUsersComponent} from './all-users/all-users.component';
+import {HomeComponent} from './home/home.component';
+import {AdminHomeComponent} from './admin-home/admin-home.component';
+import {BlogCrdComponent} from './home/blog-crd/blog-crd.component';
+import {BlogExpComponent} from './home/blog-exp/blog-exp.component';
+import {CreateBlogComponent} from './home/create-blog/create-blog.component';
+
+import { TokenCheckGuard } from './guards/token-check.guard';
+import { RoleCheckGuard } from './guards/role-check.guard';
 
 const routes: Routes = [
-  {
-    path:'create',
-    component: CreateUserComponent
-  },
   {
     path:'',
     component: LoginComponent
   },
   {
-    path:'all',
-    component: AllUsersComponent
+    path:'home',
+    component: HomeComponent,
+    children: [
+      {
+        path:'crd',
+        component: BlogCrdComponent
+      },
+      {
+        path:'exp',
+        component: BlogExpComponent
+      },
+      {
+        path:'create-blog',
+        component: CreateBlogComponent
+      }
+    ]
+  },
+  {
+    path:'admin',
+    component:AdminHomeComponent
+  },
+  {
+    path:'create',
+    component: CreateUserComponent,
+    canActivate: [TokenCheckGuard, RoleCheckGuard]
+  },
+  {
+    path:'all-users',
+    component: AllUsersComponent,
+    canActivate: [TokenCheckGuard, RoleCheckGuard]
   }
 ];
 
